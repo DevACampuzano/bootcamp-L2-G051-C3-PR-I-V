b350c4c9dd8ea7b9704a77a35d9f09295a98228b
Andrés Campuzano <andrescampu1@gmail.com>
2025-04-29 21:53:46 -0500

    update dashboard

---
+++ 04-api-peliculas/src/movieDB.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+//npm run dev
+const movieDb = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    language: "es-ES",
+  },
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmZkZWIyNGVkZDI1NGVkMWY4ZDhhNTk5N2Y2ODNlYSIsIm5iZiI6MTY2OTUxODQ4My4zMzUsInN1YiI6IjYzODJkNDkzMWIxNTdkMDA4NWViNTU2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c9LZ80tL1mBpC3rF6HNIOgnp6nOXfljX4alnh1CRsnU",
+  },
+});
+
+export default movieDb;
--- 04-api-peliculas/src/pages/dashboard/index.tsx
+++ 04-api-peliculas/src/pages/dashboard/index.tsx
@@ -1,3 +1,54 @@
+import { useState, useEffect } from "react";
+import { IResponseGetMovies } from "./interface";
+import movieDb from "../../movieDB";
+import { Box, Stack, Typography } from "@mui/material";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [list, setList] = useState<IResponseGetMovies>({
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  });
+  const getMovies = async () => {
+    try {
+      const rest = await movieDb
+        .get<IResponseGetMovies>("/movie/popular")
+        .then((res) => res.data);
+
+      setList(rest);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="start"
+      flexWrap={"wrap"}
+      gap={2}
+      minHeight="100vh"
+      p={5}
+    >
+      {list.results.map((item) => (
+        <Stack>
+          {item.poster_path !== null && (
+            <Box
+              component="img"
+              src={"https://image.tmdb.org/t/p/original" + item.poster_path}
+              width={300}
+            />
+          )}
+          <Typography color="#fff">{item.title}</Typography>
+          <Typography color="#fff">{JSON.stringify(item)}</Typography>
+        </Stack>
+      ))}
+    </Stack>
+  );
 };
